aab2b8b70dd5489c18a7623c2b08491ce911ed0a
Ali Gurur Yaman <aligururyaman@gmail.com>
2024-06-20 17:37:49 +0300

    v0.01

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import Image from "next/image"
+import pp from "../utils/pp.jpg"
 
 function About() {
   return (
-    <div>About</div>
+    <div className='flex flex-row w-[100%] items-center px-24 gap-48  h-[40rem] mt-10 bg-slate-900'>
+      <div className="">
+        <Image src={pp} height={600} width={300} className="rounded-2xl" />
+      </div>
+      <div className="w-[50%] space-y-6">
+        <p>Abdullah Küçükkılınç</p>
+        <p>İç Mimar</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus tenetur molestias soluta explicabo maiores facere vel, sequi quasi consequuntur accusantium perspiciatis similique eum harum nesciunt vitae dolore autem consectetur magni.</p>
+      </div>
+
+
+    </div>
   )
 }
 
--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import mainTop from "./utils/mainTop.png"
 import mainMobile from "./utils/mobile.png"
+import Footer from '@/components/Footer';
 
 export default function Home() {
   const [fadeIn, setFadeIn] = useState(false);
@@ -13,15 +14,18 @@ export default function Home() {
   }, []);
 
   return (
-    <main className='px-24'>
-      <div className={`transition-opacity duration-1000 ${fadeIn ? 'opacity-100' : 'opacity-0'} py-10`}>
+    <main className='xl:px-24 px-5'>
+      <div className={`transition-opacity duration-1000 ${fadeIn ? 'opacity-100' : 'opacity-0'} xl:py-10 py-12`}>
         <h1 className='text-6xl'>Yeni nesil Mimarlık...</h1>
       </div>
       <div className='hidden xl:flex'>
         <Image src={mainTop} height={700} />
       </div>
       <div className='xl:hidden'>
-        <Image src={mainMobile} height={700} />
+        <Image src={mainMobile} height={900} />
+      </div>
+      <div>
+        <Footer />
       </div>
     </main>
   );
