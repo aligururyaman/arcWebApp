a35c7d5beb24c088891a773bc521296e607d80a2
Ali Gurur Yaman <aligururyaman@gmail.com>
2024-06-21 15:29:48 +0300

    shadcn coursel

--- components/ProjectComp.jsx
+++ components/ProjectComp.jsx
@@ -1,5 +1,14 @@
-import React from 'react';
-import Slider from 'react-slick';
+'use client';
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
+import React, { useEffect, useState } from 'react';
 import mimar1 from '../app/utils/mimar1.jpg';
 import mimar2 from '../app/utils/mimar2.jpg';
 import mimar3 from '../app/utils/mimar3.jpg';
@@ -10,19 +19,22 @@ import Image from 'next/image';
 
 
 function ProjectComp() {
-  const settings = {
-    dots: true,
-    lazyLoad: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    initialSlide: 2,
-    autoplay: true,
-    speed: 4000,
-    autoplaySpeed: 4000,
-    pauseOnHover: true
-  };
+  const [api, setApi] = useState(null);
+  const [current, setCurrent] = useState(0);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
 
   const projects = [
     {
@@ -52,17 +64,25 @@ function ProjectComp() {
   ];
 
   return (
-    <div className='container mx-auto m-5'>
-      <Slider {...settings}>
-        {projects.map((project, index) => (
-          <div key={index} className='w-48 h-[35rem] rounded-lg relative'>
-            <Image src={project.img} alt={project.name} layout="fill" objectFit="cover" className="rounded-lg" />
-            <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white p-2 text-center rounded-b-lg">
-              <h1 className='text-5xl'>{project.name}</h1>
-            </div>
-          </div>
-        ))}
-      </Slider>
+    <div className='container mx-auto my-5 h-full'>
+      <Carousel setApi={setApi} className="w-full h-full" opts={{ loop: true, autoplay: true, autoplayInterval: 2000, direction: "right", transitionDuration: 1000, showControls: false, pauseOnHover: true, keyboardNavigation: true }} >
+        <CarouselContent className="h-full"  >
+          {projects.map((project, index) => (
+            <CarouselItem key={index} className="h-full" >
+              <Card className="relative h-full border-accent border-2">
+                <CardContent className="flex h-[35rem] items-center justify-center p-6 ">
+                  <Image src={project.img} alt={project.name} layout="fill" objectFit="cover" className="rounded-lg" />
+                  <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white p-2 text-center rounded-b-lg">
+                    <h1 className="text-4xl">{project.name}</h1>
+                  </div>
+                </CardContent>
+              </Card>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
   );
 }
