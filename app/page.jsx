a5dcb84de677cdd8007f103f9963de595ceada90
Ali Gurur Yaman <aligururyaman@gmail.com>
2024-06-24 18:17:00 +0300

    mainpage

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import mainTop from "./utils/mainTop.png"
 import mainMobile from "./utils/mobile.png"
+import mainBg from "./utils/glam.jpg"
 
 export default function Home() {
   const [fadeIn, setFadeIn] = useState(false);
@@ -14,11 +15,20 @@ export default function Home() {
 
   return (
     <main className='xl:px-24 px-5'>
-      <div className={`transition-opacity duration-1000 ${fadeIn ? 'opacity-100' : 'opacity-0'} xl:py-10 xl:mx-20 py-12`}>
+      {/* <div className={`transition-opacity duration-1000 ${fadeIn ? 'opacity-100' : 'opacity-0'} xl:py-10 xl:mx-20 py-12 absolute bg-primary opacity-40 rounded-xl`}>
         <h1 className='text-6xl'>Yeni Nesil Mimarlık...</h1>
+      </div> */}
+
+      <div className='absolute p-28'>
+        <div className='w-[40rem] h-[40rem] bg-accent absolute rounded-full opacity-60'>
+        </div>
+        <div className={`transition-opacity duration-1000 ${fadeIn ? 'opacity-100' : 'opacity-0'} xl:py-10 mx-10 absolute mt-40`}>
+          <h1 className='text-6xl'>Yeni Nesil Mimarlık.</h1>
+        </div>
       </div>
       <div className='hidden xl:flex'>
-        <Image src={mainTop} height={700} />
+        <Image src={mainBg} height={700} className='rounded-xl w-full' />
+        <div></div>
       </div>
       <div className='xl:hidden'>
         <Image src={mainMobile} height={900} />
