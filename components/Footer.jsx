603ed73f881d5044f2c6515e359a08c33a16d64a
Ali Gurur Yaman <aligururyaman@gmail.com>
2024-06-26 11:23:37 +0300

    v.01

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,6 @@
 'use client';
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { Button } from './ui/button';
 import {
   Dialog,
   DialogContent,
@@ -14,6 +13,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useLogin } from '@/app/LoginContext';
+import { Button } from './ui/button';
 
 function Footer() {
   const [userName, setUserName] = useState("");
